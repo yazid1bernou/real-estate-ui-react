1ffb3b1104ca6db00c185675ed6f137f373923ac
unknown <yazid1bernou@gmail.com>
2024-03-07 15:02:38 +0000

    navbar css html

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,19 @@ function Navbar () {
             </div>
             <div className="right">
                 <a href=''>Sign in</a>
-                <a href=''>Sign up</a>
+                <a href='' className='register'>Sign up</a>
+                <div className="meneIcon">
+                    <img src="/menu.png" alt="" />
+                </div>
+                <div className="menu">
+                    <a href=''>Home</a>
+                    <a href=''>About</a>
+                    <a href=''>Contacts</a>
+                    <a href=''>Agents</a>
+                    <a href=''>Sign in</a>
+                    <a href=''>Sign up</a>
+
+                </div>
             </div>
         </nav>
     )
