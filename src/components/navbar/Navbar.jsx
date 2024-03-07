99ab3d2edd2abe3d779b9d055fa6c8a16bb8115e
unknown <yazid1bernou@gmail.com>
2024-03-07 09:25:44 +0000

    create navbar structure

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,20 @@ import  './navbar.scss';
 function Navbar () {
     return (
         <nav>
-            <div className="left">Left</div>
-            <div className="right">Right</div>
+            <div className="left">
+                <a href='/' className='logo'> 
+                     <img src="/logo.png" />
+                     <span>YazidEstate</span>
+                </a>
+                <a href=''>Home</a>
+                <a href=''>About</a>
+                <a href=''>Contacts</a>
+                <a href=''>Agents</a>
+            </div>
+            <div className="right">
+                <a href=''>Sign in</a>
+                <a href=''>Sign up</a>
+            </div>
         </nav>
     )
 }
