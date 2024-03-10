33ed3846f858dd633fcf7ae190e96a8a4d6497d8
unknown <yazid1bernou@gmail.com>
2024-03-10 11:22:29 +0000

    begin to create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './layout.scss';
 import Navbar from "./components/navbar/Navbar";
+import homePage from "./pages/homePage";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
         <div className='layout'> 
-           <Navbar />
+           <div className="navbar">
+              <Navbar />
+           </div>
+           <div className="content">
+              <homePage />
+           </div>
+          
         </div>
     </>
   )
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,11 @@
+import "./homePage.scss";
+
+function homePage () {
+    return (
+        <div className="homePage">
+              home Page
+        </div>
+    )
+}
+
+export default homePage ;
