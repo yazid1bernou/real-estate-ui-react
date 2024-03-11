aeda78441fa5f5313bedb7873fc127b50450a712
unknown <yazid1bernou@gmail.com>
2024-03-11 09:27:12 +0000

    creation the differents elements of

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -3,7 +3,16 @@ import "./searchBar.scss";
 function SearchBar () {
     return (
         <div className="searchBar">
-              Search Bar
+            <div className="type">
+                <button>Buy</button>
+                <button>Rent</button>
+            </div>
+            <form action="">
+                <input type="text" name="location" placeholder="City location"/>
+                <input type="number" name="minPrice" min={0} max={1000000} placeholder="Min price"/>
+                <input type="number" name="maxPrice" min={0} max={1000000} placeholder="Max price"/>
+                <button><img src="/search.png" alt="" /></button> 
+            </form>
         </div>
       
     )
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -8,6 +8,20 @@ function HomePage () {
                     <h1 className="title"> Find Real Estate & Get Your Dream Place</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis labore, incidunt dicta nesciunt facere repellat provident, quasi dignissimos temporibus error ratione deserunt libero molestiae nulla voluptas maxime minima rem magnam.</p>
                     <SearchBar />
+                    <div className="boxes">
+                        <div className="boxe">
+                          <h1>+16</h1>
+                          <h2>Years of experience</h2>
+                        </div>
+                        <div className="boxe">
+                          <h1>200</h1>
+                          <h2>Award gained</h2>
+                        </div>
+                        <div className="boxe">
+                          <h1>2000</h1>
+                          <h2>Property ready</h2>
+                        </div>
+                    </div>
                  </div> 
               </div>
               <div className="imgContainer"> 
